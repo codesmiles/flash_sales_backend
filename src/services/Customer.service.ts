45e6e3c6d8cee1a701ea6cdacefbc429306e2014
Nwogu Michael Daberechi <codesmiles@Nwogus-MacBook-Pro.local>
2025-03-27 04:19:05 +0100

    implement buyPromoProductService

--- src/services/Customer.service.ts
+++ src/services/Customer.service.ts
@@ -3,7 +3,7 @@ import { type ClientSession } from "mongoose";
 import type { IProduct, ISale } from "../Interface";
 
 type BuyProductType = { productId: string, units: number, userId:string }
-type BuyPromoProductType = { productId: string, promoId: string, units: number };
+type BuyPromoProductType = { productId: string, promoId: string, units: number, userId: string };
 export type AvailablePromoProductType = { promoId: string, per_page: number, current_page: number };
 abstract class CustomerAbstract {
     abstract buyProduct(payload: BuyProductType, session: ClientSession): Promise<ISale>;
@@ -15,6 +15,7 @@ abstract class CustomerAbstract {
 export class CustomerService extends CustomerAbstract {
     async buyProduct(payload: BuyProductType, session: ClientSession): Promise<ISale> {
 
+        
         // find product by id and check if it exists
         // check the available unit left and return an error if its at 0 or after deducting the units it will be less than 0
         // update the available units
@@ -23,8 +24,9 @@ export class CustomerService extends CustomerAbstract {
             { $inc: { units: -payload.units } },
             {  session, new: true, select:"price" }
         );
-        if (!product) throw new Error("Invalid product | insufficient inventory | product not found");
-        
+        if (!product) throw new Error(`Invalid product | insufficient inventory | product not found | ${product}`);
+
+
         // create a sales receipt
         const sales_payload = {
             is_promo: false,
@@ -46,14 +48,53 @@ export class CustomerService extends CustomerAbstract {
         
         // find promo by id and check if it's active
         const findPromo = await Promo.findOne({ _id: payload.promoId, is_active:true }).session(session);
-        if (!findPromo) throw new Error("unable to find promo")
+        if (!findPromo) throw new Error(`unable to find promo | Promo Is inactive | ${findPromo}`)
         
         // find product by id check if is_promo is true and check both units and available units
         // if assigned promo unit or regular === 0 or regular unit <= promo unit update the the is_promo to false and return an error
         // If the purchasing promo unit when deducted is going to be less than 0 return an error
         // Update the product according to the units create a sales receipt for the promo purchase and return the sales receipts
-        // return a success and commit the transaction
-        // if any error occurs rollback the transaction and return an error message
+        const update_product = await Product.findOneAndUpdate(
+            {
+                _id: payload.productId,
+                is_promo: true,
+                "promo_details.promo_id": payload.promoId,
+                "promo_details.units": { $gte: payload.units },
+                $expr: {
+                    $gte: ["$units", "$promo_details.units"]
+                }
+            },
+            {
+                $inc: { "promo_details.units": -payload.units },
+                $set: {
+                    is_promo: {
+                        $cond: {
+                            if: { $eq: [{ $subtract: ["$promo_details.units", payload.units] }, 0] },
+                            then: false, 
+                            else: true 
+                        }
+                    }
+                }
+            },
+            { session, new: true, select: "promo_details.price" })
+
+        if (!update_product) throw new Error(`Invalid product | insufficient inventory | product not found | ${update_product}`);
+
+
+        // create a sales receipt
+        const sales_payload = {
+            is_promo: true,
+            user_id: payload.userId,
+            quantity: payload.units,
+            product_id: payload.productId,
+            price: payload.units * parseFloat(update_product.price),
+        }
+
+        const new_sale = new Sale(sales_payload);
+        await new_sale.save({ session });
+
+        // return a successful transaction and sales receipt
+        return new_sale as ISale;
     }
 
     async availablePromoProduct(payload: AvailablePromoProductType, session?: ClientSession): Promise<IProduct[]> {
